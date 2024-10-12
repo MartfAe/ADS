d713494b5d0348a30fcd55337f3941091ca48ec4
MartfAe <anaemilia@aluno.ufrb.edu.br>
2024-10-12 20:47:04 -0300

    cabeçalho

---
+++ Projeto Escola/ProjetoEscola.c
@@ -0,0 +1,7 @@
+    /*Projeto Escola do semestre 24.2 da disciplina INF029.
+      Desenvolvido por: Ana Emília Lobo, Marcus Vinícius da Fonseca, Roberto Silva. */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
