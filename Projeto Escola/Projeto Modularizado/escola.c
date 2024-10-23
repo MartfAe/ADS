19c727a30f31964f5c0ca3125b71f4afe71162a5
MartfAe <anaemilia@aluno.ufrb.edu.br>
2024-10-23 10:32:46 -0300

    att modular

--- Projeto Escola/Projeto Modularizado/escola.c
+++ Projeto Escola/Projeto Modularizado/escola.c
@@ -71,18 +71,91 @@ void setDisciplina(materias disciplinas[], int index, materias novaDisciplina) {
 }
 
 // Implementação da função de cadastro de pessoa
-void cadastrarPessoa(pessoas *cadastro, int maxPessoas, char tipoPessoa) {
-    for (int i = 0; i < maxPessoas; i++) {
-        if (cadastro[i].matricula == 0) {
+void cadastrarPessoa(pessoas *cadastro, int maxPessoas, char tipoPessoa)  {
+    int i;
+    for (i = 0; i < maxPessoas; i++) {
+        if (cadastro[i].matricula == 0) {  // Verifica se o campo matrícula está vazio
+            if (tipoPessoa == 1) {
+                printf("Cadastro de Aluno:\n\n");
+            } else if (tipoPessoa == 2) {
+                printf("Cadastro de Professor:\n");
+            }
+
             printf("Informe o nome:\n");
             scanf(" %[^\n]", cadastro[i].nome);
-            // Restante do cadastro
-            break;
+
+            // Substituir cadastro de matrícula por autoincremento
+            printf("Informe a matrícula:\n");
+            scanf("%d", &cadastro[i].matricula);
+
+            printf("Informe o CPF:\n");
+            scanf("%s", cadastro[i].cpf);
+
+            printf("Informe o sexo (M/F):\n");
+            scanf(" %c", &cadastro[i].sexo);  // Corrige o uso do scanf para ler char corretamente
+
+            printf("Informe a data de nascimento separada por um espaço (dd mm aaaa):\n");
+            scanf("%d %d %d", &cadastro[i].aniversario.dia, &cadastro[i].aniversario.mes, &cadastro[i].aniversario.ano);//verificar se é possível substituir por gets
+
+            if (tipoPessoa == 1) {
+                printf("Aluno cadastrado com sucesso!\n");
+            } else if (tipoPessoa == 2) {
+                printf("Professor cadastrado com sucesso!\n");
+            }
+
+            break;  // Sai do loop após o cadastro
+        }
+    }
+    // Se o loop terminar sem encontrar uma posição vazia, exibe mensagem
+    if (i == maxPessoas) {
+        if (tipoPessoa == 1) {
+            printf("Não há espaço para cadastrar mais alunos.\n");
+        } else if (tipoPessoa == 2) {
+            printf("Não há espaço para cadastrar mais professores.\n");
         }
     }
 }
 
 // Implementação da função de cadastro de disciplina
 void cadastrarDisciplina(materias *disciplinas, int max_Disciplinas, pessoas *professores, int max_Professores) {
-    // Aqui está o código para cadastro de disciplinas
+  for(int i = 0; i < max_Disciplinas; i++){
+    if(disciplinas[i].codigo[0 == '\0']){ //Verifica se a disciplina foi cadastrada
+      printf ("Cadastro de Disciplina:\n");
+
+      printf("Informe o nome da disciplina:\n");
+      scanf("%[/n]", &disciplinas[i].nome);
+
+      printf("Informe o código da disciplina (máx 8 caracteres):\n");
+      scanf("%s", &disciplinas[i].codigo);
+
+      printf("Informe o semestre da disciplina:\n");
+      scanf("%d", &disciplinas[i].semestre);
+
+      printf("Selecione o professor pelo número da matrícula: \n");
+      for(int j = 0; j<max_Professores; j++){
+        printf("%d - %s .\n", professores[j].matricula, professores[j].nome);
+      }
+      int matriculaProf; 
+      scanf("%d", &matriculaProf);
+
+      //Procura o professor pela matricula e atrela a disciplina
+
+      int encontrou=0;
+      for(int j = 0; j < Max_Professores; j++){
+        if(professores[j].matricula == matriculaProf){
+          strcpy(disciplinas[i].professor, professores[j].nome);
+          encontrou = 1 ;
+          break;
+        }
+      }
+
+      if(!encontrou){
+        printf("Professor não encontrado.\n");
+      }else{
+        disciplinas[i].numAlunos=0;
+        printf("Disciplina cadastrada com sucesso!\n");
+      }
+
 }
+  }
+  }
